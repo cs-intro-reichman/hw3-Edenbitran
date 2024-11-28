05f5fbf9549ca193e9ed60707355075c8fb0cdc4
Edenbitran <eden.bitran@post.runi.ac.il>
2024-11-28 18:53:08 +0200

    Anagram -random

--- Anagram.java
+++ Anagram.java
@@ -5,38 +5,34 @@ public class Anagram {
 	public static void main(String args[]) {
 		// Tests the isAnagram function.
 
-		//   System.out.println(isAnagram("silent", "listen")); // true
-		//   System.out.println(isAnagram("William Shakespeare", "I am a weakish speller")); // true
-		//   System.out.println(isAnagram("Madam Curie", "Radium came")); // true
-		//   System.out.println(isAnagram("Tom Marvolo Riddle", "I am Lord Voldemort")); // true
+		    System.out.println(isAnagram("silent", "listen")); // true
+		    System.out.println(isAnagram("William Shakespeare", "I am a weakish speller")); // true
+		    System.out.println(isAnagram("Madam Curie", "Radium came")); // true
+		    System.out.println(isAnagram("Tom Marvolo Riddle", "I am Lord Voldemort")); // true
 
 
 		// Tests the preProcess function.
-		//System.out.println(preProcess("What? No way!!!"));
-		  //String str1 = preProcess("");
-		  //System.out.println(str1);
-		  //String str2 = preProcess("");
-		  //System.out.println(str2);
-		 //System.out.println(isAnagram(str1, str2));
+		String str1 = preProcess("no Way");
+		//System.out.println(str1);
+		String str2 = preProcess("What?");
+		//System.out.println(str2);
+		System.out.println(isAnagram(str1, str2));
 
-
-		// System.out.println("********************");
 		//// Tests the randomAnagram function.
-		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
-		System.out.println(randomAnagram("hello"));
-		 
-		// // Performs a stress test of randomAnagram
-		 String str = "1234567";
-		 Boolean pass = true;
-		 //// 10 can be changed to much larger values, like 1000
-		 for (int i = 0; i < 10; i++) {
-		 String randomAnagram = randomAnagram(str);
-		 System.out.println(randomAnagram);
-		 pass = pass && isAnagram(str, randomAnagram);
-		 if (!pass)
-		 break;
-		 }
-		 System.out.println(pass ? "test passed" : "test Failed");
+		 System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
+		 System.out.println(randomAnagram("hello"));
+		 // // Performs a stress test of randomAnagram
+		  String str = "1234567";
+		  Boolean pass = true;
+		  //// 10 can be changed to much larger values, like 1000
+		  for (int i = 0; i < 10; i++) {
+		  String randomAnagram = randomAnagram(str);
+		  System.out.println(randomAnagram);
+		  pass = pass && isAnagram(str, randomAnagram);
+		  if (!pass)
+		  break;
+		  }
+		  System.out.println(pass ? "test passed" : "test Failed");
 	}
 
 	// Returns true if the two given strings are anagrams, false otherwise.
@@ -46,25 +42,14 @@ public class Anagram {
 		String cStr2 = preProcess(str2);
 		String abcS1 = (abc(preProcess(cStr1)));
 		String abcS2 = (abc(preProcess(cStr2)));
-		for (int i = 0; i < cStr1.length(); i++) {
-			if (cStr1.charAt(i) != ' ') {
-				cStr1 += cStr1.charAt(i);
-			}
-		}
-		for (int i = 0; i < cStr2.length(); i++) {
-			if (cStr2.charAt(i) != ' ') {
-				cStr2 += cStr2.charAt(i);
-			}
-		}
-		//check the length of the strings
-		for (int i = 0; i < cStr1.length(); i++) {
-			if (cStr1.length() == cStr2.length()) {
+		
+		for (int i = 0; i < abcS1.length(); i++) {
+			if (abcS1.length() == abcS2.length()) {
 				bool = true;
 			} else {
 				return false;
 			}
 		}
-		//System.out.println(cStr1 + "-> " + abcS1 + "\n" + cStr2 + "-> " + abcS2);
 		if (abcS1.equals(abcS2)) {
 			bool = true;
 		}
@@ -79,7 +64,7 @@ public class Anagram {
 		String abcS = "";
 		for (char ch = 'a'; ch <= 'z'; ch++) {
 			for (int i = 0; i < str.length(); i++) {
-				if (ch==str.charAt(i)) {
+				if (ch==str.charAt(i)&&ch!= ' ') {
 					abcS += ch;
 				}
 			}
